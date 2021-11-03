3aa95e1973a8a1a7f7beece2b220aefd49689b03
KevsonDoc <kevsonfilipesantos@gmail.com>
2021-11-03 11:53:27 -0300

    [feat]: added styles components environment

---
+++ src/assets/styles/global.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --primary: #FFFFFF;
+    --secondary: #B18B5A;
+    --background: #FCF8F3;
+    --text-title: #3A3A3A;
+    --text-paragraph: #616161;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  html, body, #__next {
+    height: 100%;
+  }
+
+  body {
+    font-family: 'ABeeZee',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  p {
+    font-size: 2rem;
+  }
+`
+
+export default GlobalStyle
